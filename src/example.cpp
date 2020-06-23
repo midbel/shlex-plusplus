25119e9237780826308beec81e71ced5bbc88201
midbel <brun.nicolas21@gmail.com>
2020-06-23 20:32:50 +0200

    split args on spaces (space or tab)

--- src/example.cpp
+++ src/example.cpp
@@ -4,9 +4,13 @@
 int main(int argc, char** argv) {
   std::vector<std::string> args;
   for (int i = 1; i < argc; i++) {
+    std::cout << argv[i] << std::endl;
     args = shlex::shlex(argv[i]);
     for (auto a: args) {
-      std::cout << a << std::endl;
+      std::cout << " " << a << std::endl;
+    }
+    if (i < argc-1) {
+      std::cout << "---" << std::endl;
     }
   }
 }
--- src/shlex.cpp
+++ src/shlex.cpp
@@ -1,8 +1,28 @@
 #include "shlex.h"
 
 namespace shlex {
+
+  const char space = ' ';
+  const char tab = '\t';
+
+  bool is_space(char ch) {
+    return ch == space || ch == tab;
+  }
+
   std::vector<std::string> shlex(std::string str) {
     std::vector<std::string> vs;
+    auto it = str.begin();
+    while(it < str.end()) {
+      auto prev = it;
+      while (it < str.end() && !is_space(*it)) {
+        it = next(it);
+      }
+      vs.push_back(std::string(prev, it));
+      while (is_space(*it)) {
+        it = next(it);
+      }
+      prev = it;
+    }
     return vs;
   }
 }
